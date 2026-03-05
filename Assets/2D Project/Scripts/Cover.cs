357250ccb0d4725451b7118dcdf99dddd3c80430
nick-hansen-sd <nhansen@csumb.edu>
2026-03-04 22:45:23 -0800

    Covers

---
+++ Assets/2D Project/Scripts/Cover.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Cover : MonoBehaviour
+{
+    public float health = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            health -= 1;
+            Destroy(collision.gameObject);
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
