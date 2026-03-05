a399c513c706bd295da0944ad80978078a7753f8
nick-hansen-sd <nhansen@csumb.edu>
2026-03-05 00:02:40 -0800

    Enemy group movement system v2

--- Assets/2D Project/Scripts/EnemyGroupManager.cs
+++ Assets/2D Project/Scripts/EnemyGroupManager.cs
@@ -4,6 +4,7 @@ public class EnemyGroupManager : MonoBehaviour
 {
     public float speed = 5f;
     public float speedMultiplier = 1.2f;
+    bool triggeredThisStep = false; //Prevents rows from moving down multiple times per frame
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,12 +16,18 @@ public class EnemyGroupManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        triggeredThisStep = false;
         Vector3 newPosition = transform.position - new Vector3(speed, 0, 0) * Time.deltaTime;
         transform.position = newPosition;
     }
 
     void OnEnemyReachedEdge(float rowHeight)
     {
+        if (triggeredThisStep)
+        {
+            return;
+        }
+        triggeredThisStep = true;
         speed = -speed;
         transform.position = transform.position - new Vector3(0, rowHeight, 0);
     }
