170162bd90519789010d6f41ff0493bf9d3d6970
nick-hansen-sd <nhansen@csumb.edu>
2026-03-11 23:01:58 -0700

    Credits

--- Assets/2D Project/Scripts/SceneLoader.cs
+++ Assets/2D Project/Scripts/SceneLoader.cs
@@ -1,10 +1,25 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
     public void LoadGame()
     {
-        SceneManager.LoadScene("Schmup");
+        StartCoroutine(_LoadGame());
+
+        IEnumerator _LoadGame() {
+            AsyncOperation loadOperation = SceneManager.LoadSceneAsync("Schmup");
+            while (!loadOperation!.isDone) yield return null;
+
+            //Wait until scene is loaded and ready and then find the player
+            GameObject playerObj = GameObject.Find("Player");
+            Debug.Log(playerObj.name);
+        }
+        
     }
 }
